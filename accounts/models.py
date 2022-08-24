e60681fda267132a0c88bc137ffd717f9e93a347
Jimin Kim <wweww1596@likelion.org>
2022-08-24 23:33:40 +0900

    setting, api test

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=6)
--- apply/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'date', 'place', 'is_public', 'personnel']
-    list_display_links = ['title']
-    list_filter = ['created_at', 'is_public']
-    search_fields = ['title']
