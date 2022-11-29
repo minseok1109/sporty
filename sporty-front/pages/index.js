82aea3e8aed192260cb6122434f036fa83105f02
DongHyun <danny001006@naver.com>
2022-11-29 19:32:20 +0900

    DatePicker Error fix

--- sporty-front/pages/index.js
+++ sporty-front/pages/index.js
@@ -1,11 +1,12 @@
 import Profile from "../components/Profile";
-import BasketPostNew from "../components/PostFroms/BasketPostNew";
+import BasketPost from "../components/PostForms/BasketPost";
 import BasketPostList from "../components/PostLists/BasketPostList/BasketPostList";
+
 
 export default function Home() {
   return (<>
     <Profile />
-    <BasketPostNew></BasketPostNew>
+    <BasketPost></BasketPost>
     <BasketPostList></BasketPostList>
 
   </>
