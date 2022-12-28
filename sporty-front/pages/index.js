2ff23c174ec1d161516e2f69263782ac3e68e3c6
minseok1109 <park485201@naver.com>
2022-12-28 16:02:15 +0900

    feat: add tabs with exercise

--- sporty-front/pages/index.js
+++ sporty-front/pages/index.js
@@ -1,10 +1,35 @@
 import PostList from "../components/PostLists/PostList";
+import { Tabs } from "antd";
+
 export default function Home() {
+  const postListArr = [
+    {
+      label: "농구",
+      key: "1",
+      children: <PostList postListUrl={"basketposts"} />,
+    },
+    {
+      label: "걷기",
+      key: "2",
+      children: <PostList postListUrl={"workposts"} />,
+    },
+    {
+      label: "기타",
+      key: "3",
+      children: <PostList postListUrl={"freeposts"} />,
+    },
+  ];
   return (
-    <>
-      <PostList postListUrl={"basketposts"} />
-      <PostList postListUrl={"workposts"} />
-      <PostList postListUrl={"freeposts"} />
-    </>
+    <Tabs
+      defaultActiveKey="1"
+      centered
+      items={postListArr.map((component, _) => {
+        return {
+          label: component.label,
+          key: component.key,
+          children: component.children,
+        };
+      })}
+    />
   );
 }
