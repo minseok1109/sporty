163222151dac6fb0756cbffac77fb9d5413bb33d
DongHyun <danny001006@naver.com>
2023-02-07 01:30:24 +0900

    chat test

--- sporty-front/components/Chat/Chat.js
+++ sporty-front/components/Chat/Chat.js
@@ -5,18 +5,18 @@ import { useStoreState } from "../../store";
 import jwt_decode from "jwt-decode";
 
 export default function Chat() {
-  const store = useStoreState();
-  const { jwtToken } = store;
-  useEffect(() => {
-    const header = { Authorization: `JWT ${jwtToken}` };
-    const getUserData = () => {
-      fetch("http://localhost:8000/accounts/api/user", header)
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-    };
-    getUserData();
-  }, []);
-  console.log(jwt_decode(jwtToken));
+  // const store = useStoreState();
+  // const { jwtToken } = store;
+  // useEffect(() => {
+  //   const header = { Authorization: `JWT ${jwtToken}` };
+  //   const getUserData = () => {
+  //     fetch("http://localhost:8000/accounts/api/user", header)
+  //       .then((response) => response.json())
+  //       .then((json) => console.log(json));
+  //   };
+  //   getUserData();
+  // }, []);
+  // console.log(jwt_decode(jwtToken));
   const APP_ID = "DB06BA41-2A05-40AE-8E69-BB26D88F20D8";
 
   return (
@@ -25,9 +25,10 @@ export default function Chat() {
         <SendbirdApp
           // Add the two lines below.
           appId={APP_ID} // Specify your Sendbird application ID.
-          //   userId={jwtToken && jwt_decode(jwtToken).payload.username}
-          //   nickname={jwtToken && jwt_decode(jwtToken).payload.username} // Specify your user ID.
-          // nickname={nickname}
+          userId={'test'}
+        // userId={jwtToken && jwt_decode(jwtToken).payload.username}
+        // nickname={jwtToken && jwt_decode(jwtToken).payload.username}
+        // nickname={nickname}
         />
       </div>
     </>
