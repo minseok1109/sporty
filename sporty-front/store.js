332230f0bd98ed9b2d2f9d42a6e58f1337c0a25c
minseok1109 <park485201@naver.com>
2023-02-06 18:43:37 +0900

    delete not used file

--- sporty-front/store.js
+++
@@ -1,9 +0,0 @@
-// import { create } from "zustand";
-// const userStore = create((set) => ({
-//   user: {},
-//   accessToken: null,
-//   setUser: (user) => set({ user }),
-//   setAccessToken: (token) => set({ accessToken: token }),
-// }));
-
-// export default userStore;
